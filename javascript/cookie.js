874b3880dc898a98ceb2f41c4906185caa1baf41
luckyjing <649008270@qq.com>
2015-09-17 00:19:39 +0800

    修fix bug

--- javascript/cookie.js
+++ javascript/cookie.js
@@ -7,7 +7,7 @@ var Cookie = {
 			//查找到此cookie值
 			var cookieEnd = document.cookie.indexOf(';',cookieStart);
 			if (cookieEnd == -1){
-				//未找到分好，说明该cookie是字符串中的最后一个，则余下的字符全部为cookie的值 
+				//未找到分号，说明该cookie是字符串中的最后一个，则余下的字符全部为cookie的值 
 				cookieEnd = document.cookie.length;
 			}
 			cookieValue = decodeURIComponent(document.cookie.substring(cookieStart+cookieName.length,cookieEnd));
@@ -18,9 +18,16 @@ var Cookie = {
 	set: function (name,value,days,path,domain,secure){
 		var cookieText = encodeURIComponent(name) + '=' + encodeURIComponent(value);
 		 if (days) {
-			 var time = new Date();
-			 time.setTime(time.getTime() + days * 24 * 60 * 60 * 1000);
-			 cookieText += '; expires='+days.toGMTString();
+			 var time = null;
+			 if(days == -1){
+				 //设置过期时间，用以删除
+				time = new Date(0);
+				cookieText += '; expires='+time.toGMTString();
+			 }else{
+			    time = new Date();
+				time.setTime(time.getTime() + days * 24 * 60 * 60 * 1000);
+				cookieText += '; expires='+time.toGMTString();
+			 }
 		 }
 		 if (path) {
 			 cookieText += '; path=' + path;
@@ -33,7 +40,8 @@ var Cookie = {
 		 }
 		 document.cookie = cookieText;
 	},
+	
 	unset: function(name,path,domain,secure){
 		this.set(name,"",-1,path,domain,secure);
 	}
-}
+};
